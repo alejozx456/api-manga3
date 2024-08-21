68378de84c009095d55b232b3a78cf1acf1699f9
alejozx456 <alejozx456@gmail.com>
2024-08-20 23:03:29 -0500

    docker 3 deploy

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def scrape():
         return jsonify({'error': f'Ocurrió un error inesperado: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
